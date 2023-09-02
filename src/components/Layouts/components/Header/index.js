099e673ed530efe4f38d2171217abebea022a300
NhatNamITPRO <nhatnamit214@gmail.com>
2023-09-02 15:10:51 +0700

    update project

---
+++ src/components/Layouts/components/Header/index.js
@@ -0,0 +1,30 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+const cx = classNames.bind(styles);
+function Header() {
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="TikTok"></img>
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Tìm kiếm" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
